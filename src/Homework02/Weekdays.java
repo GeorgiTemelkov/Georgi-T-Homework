0f782bd79c09dd844b534bf6df117f8212e12648
Remus <g.temelkov88@gmail.com>
2022-05-19 21:57:52 +0300

    commit

--- src/Homework02/Weekdays.java
+++ src/Homework02/Weekdays.java
@@ -3,11 +3,13 @@ package Homework02;
 import java.util.Scanner;
 
 public class Weekdays {
-    public static void main(String[] args) {
+    public static void main (String[] args) {
+
+
 
         int days;
-        String daySuffix;
-        String dayOfWeek;
+        String daySuffix = "th";
+        String dayOfWeek = null;
 
 
         Scanner scanner = new Scanner(System.in);
@@ -17,19 +19,42 @@ public class Weekdays {
         switch (days){
             case 1:
                 daySuffix = "st";
+                dayOfWeek = "Monday";
                 break;
 
             case 2:
                 daySuffix = "nd";
+                dayOfWeek = "Tuesday";
                 break;
             case 3:
                 daySuffix = "rd";
+                dayOfWeek = "Wednesday";
+                break;
+
+            case 4:
+                dayOfWeek = "Thursday";
+                break;
+
+            case 5:
+                dayOfWeek = "Friday";
+                break;
+
+            case 6:
+                dayOfWeek = "Saturday";
+                break;
+
+            case 7:
+                dayOfWeek = "Sunday";
                 break;
 
             default:
-                daySuffix = "th";
+                daySuffix = "This is not a valid day of the week";
                 break;
         }
-
+        if (days <1 && days >7){
+            System.out.println("This is not a valid day of the week");
+        }
+        System.out.format("The %d -%s day of the week %s", days, daySuffix, dayOfWeek);
     }
 }
+
