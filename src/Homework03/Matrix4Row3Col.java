a86936f65061199f1583764a44f4b6e74875476e
Remus <g.temelkov88@gmail.com>
2022-05-27 19:36:20 +0300

    commit

---
+++ src/Homework03/Matrix4Row3Col.java
@@ -0,0 +1,12 @@
+package Homework03;
+import java.util.Scanner;
+public class Matrix4Row3Col {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+
+
+
+    }
+}
