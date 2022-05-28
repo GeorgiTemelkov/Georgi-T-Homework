5c1b0322637d0e77f12ce47946be798e09c7370f
Remus <g.temelkov88@gmail.com>
2022-05-28 14:11:42 +0300

    commit

---
+++ src/Homework03/PalindromeCheck.java
@@ -0,0 +1,22 @@
+package Homework03;
+
+import java.util.Scanner;
+public class PalindromeCheck {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please enter a word:");
+        String word = scanner.next();
+
+        char[] front = word.toCharArray();
+
+        char[] reverse = new char[0];
+
+        for (int i = front.length-1; i>=0; i--){
+            reverse.
+
+        }
+
+    }
+}
