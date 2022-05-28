50d712c38001c6ca3a07e829df10c5a888e3934c
Remus <g.temelkov88@gmail.com>
2022-05-28 18:05:40 +0300

    commit

--- src/Homework03/PalindromeCheck.java
+++ src/Homework03/PalindromeCheck.java
@@ -16,7 +16,7 @@ public class PalindromeCheck {
         for (int i = front.length-1; i>=0; i--){
             for (int j =0; j<= front.length;j++) {
 
-               reverse[j] = front.[i];
+      //         reverse[j] = front.[i];
 
             }
         }
